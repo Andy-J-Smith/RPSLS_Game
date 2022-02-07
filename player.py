38f7d7bf8ccbe417d236995f94353f1fd16cc9fd
Andy-J-Smith <98605897+Andy-J-Smith@users.noreply.github.com>
2022-02-07 14:53:04 -0600

    Initiated Player class with variables and added choose guesture function

---
+++ player.py
@@ -0,0 +1,16 @@
+from ai import Ai
+from human import Human
+
+class Player:
+
+    def __init__(self):
+        self.name = ''
+        self.wins = 0
+        self.chose_guesture = ''
+        self.list_of_guestures = ["Rock", "Paper", "Scissor", "Lizard", "Spock"]
+
+
+    def choose_guesture(self):
+        pass
+    
+        
