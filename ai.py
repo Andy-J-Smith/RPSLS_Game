e3b1dca4f0309f3138d6a88319d3734224f9606c
Andy-J-Smith <98605897+Andy-J-Smith@users.noreply.github.com>
2022-02-08 10:25:38 -0600

    Updated game file for player mode

--- ai.py
+++ ai.py
@@ -7,3 +7,10 @@ class Ai(Player):
     def __init__(self):
         self.name = ("")
         # super().__init__()
+
+
+    def choose_guesture(self):
+        cpu_gest = random.choice(self.list_of_guestures)
+        return super().choose_guesture()
+        
+
